78082f5306d7c6fbace2c2a45e7648fcb0e6b184
SkyShaym01 <houleznathan@gmail.com>
2024-11-08 09:47:18 +0100

    Create PauseMenu ajoutement touche necessaire

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public bool isPaused;
+
+    void Start(){
+        pauseMenu.SetActive(false);
+    }
+
+    void Update()
+    {
+        // Vérifiez si la touche Échap est pressée
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitter le jeu");
+        Application.Quit();
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+}
